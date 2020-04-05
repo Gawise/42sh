ffd2fec56784b7647524c1546fa5b57a32bc3a72
akkud <piti-pol-du45@hotmail.fr>
2020-04-05 13:37:45 +0200

    maj pour print d'erreur correct

--- srcs/exp/exp.c
+++ srcs/exp/exp.c
@@ -5,24 +5,33 @@
 #include "ft_printf.h"
 #include "var.h"
 
+int	exp_err(char *word)
+{
+	ft_dprintf(2, "%s: Bad Substitution\n", word);
+	ft_setvar(&cfg_shell()->env, "?", "1");
+	return (-1);
+}
+
 int	word_expansions(t_simple_cmd *cmd)
 {
 	t_list		*lst;
 	t_assignment	*assign;
 
-	if (cmd->cmd_name)
-		exp_main(&cmd->cmd_name, 0);
+	if (cmd->cmd_name && exp_main(&cmd->cmd_name, 0) < 0)
+		return (exp_err(cmd->cmd_name));
 	lst = cmd->args;
 	while (lst)
 	{
-		exp_main((char **)&lst->data, 0);
+		if (exp_main((char **)&lst->data, 0) < 0)
+			return (exp_err((char *)lst->data));
 		lst = lst->next;
 	}
 	lst = cmd->assign;
 	while (lst)
 	{
 		assign = lst->data;
-		exp_main((char **)&assign->val, 1);
+		if (exp_main((char **)&assign->val, 1) < 0)
+			return (exp_err((char *)assign->val));
 		lst = lst->next;
 	}
 	return (1);
@@ -48,11 +57,7 @@ int	exp_main(char **word, int assign)
 	}
 	ft_bzero(exp.buf, EXP_BSIZE);
 	if (parse_param_exp(word, exp) < 0)
-	{
-		ft_dprintf(2, "%s: Bad Substitution\n", *word);
-		ft_setvar(&cfg_shell()->env, "?", "1");
 		return (-1);
-	}
 	printf("Sortie exp main\nstr= [%s]\n\n", *word);
 	return (1);
 }
