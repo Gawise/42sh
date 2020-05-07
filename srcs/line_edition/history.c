41074d78404978c66921097d665f84fae0299839
ew <izi@mit.edu>
2020-05-07 19:25:15 +0200

    correctif mergee

--- srcs/line_edition/history.c
+++ srcs/line_edition/history.c
@@ -85,7 +85,7 @@ void	history_up(t_cs_line *cs)
 			ft_clear(1);
 			cs->clipb = (t_point){-1, -1};
 			cs->history = cs->history->prev;
-			t_strdel(&cs->old_history);
+			ft_strdel(&cs->old_history);
 			cs->old_history = ft_strdup((char *)cs->history->data);
 			cs->input = (char *)cs->history->data;
 			cs->line_col = ft_strlen(cs->input);
