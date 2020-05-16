77605f7a9ab3e09f7c7265134e1befcbba716da6
waldenswartz <deh@lana>
2020-05-16 19:09:01 +0200

    merge conflict2

--- srcs/line_edition/ft_prompt.c
+++ srcs/line_edition/ft_prompt.c
@@ -110,7 +110,7 @@ char	*ft_prompt(char *prompt, char *color)
 	cs = NULL;
 	if (!(cfg = cfg_shell()))
     	return (NULL);
-	if (cfg->mode == cfg->interactive)
+	if (!cfg->interactive)
 		return (read_nonint(cfg));
 	if (term_init(1, prompt) == 1 && (cs = cs_master(NULL, 0)))
 	{
