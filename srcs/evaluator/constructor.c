374f291533b249e8842dda34805c7875b3d72442
delahBinks <39902522+delahBinks@users.noreply.github.com>
2020-06-09 02:32:28 +0200

    Line edition (#165)
    * correction du \n au milieu de la string, correction du scroll dans lhistorique (dernier element fleche du bas), ajout de la couleur du coup ft_prompt prend un char * en plus en param, correction doublons historique, amelioration init_shell notamment pour : ./21sh 0>&-
    
    * corrections issues SLE, leak -> input_map
    
    * correctifs historique
    
    * correction du merge
    
    * correctif merge
    
    * correctif mergee
    
    * amelioration hsitorique, ctrl-c, position du curseur, corrections leaks
    
    * amelioration ft_prompt, resolution stdout close echo, normage
    
    * norme
    
    Co-authored-by: delahBinks <ambelghi@student.42.fr>
    Co-authored-by: ambelghi <izi@mit.edu>

--- srcs/evaluator/constructor.c
+++ srcs/evaluator/constructor.c
@@ -31,11 +31,11 @@ void	cmd_to_job(t_cfg *shell, t_job *job, t_list *s_cmd, char *cmd)
 	cmd_to_process(&job->process, s_cmd);
 	job->cur = 32;
 	job->cmd = ft_strdup(cmd);
-	job->std[0] = 256;
+	job->std[0] = 253;
 	do_my_dup2(STDIN_FILENO, job->std[0]);
-	job->std[1] = 257;
+	job->std[1] = 254;
 	do_my_dup2(STDOUT_FILENO, job->std[1]);
-	job->std[2] = 258;
+	job->std[2] = 255;
 	do_my_dup2(STDERR_FILENO, job->std[2]);
 	term_create_eval(&shell->term_origin, &job->term_eval);
 }
