7237bcf4014b708965d7d2cc8f75c510e34bb248
delahBinks <ambelghi@student.42.fr>
2020-06-17 21:53:19 +0200

    save me add edit et exec (pas fini du tt)

--- srcs/builtins/fc.c
+++ srcs/builtins/fc.c
@@ -6,7 +6,7 @@
 /*   By: ambelghi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/11 18:32:54 by ambelghi          #+#    #+#             */
-/*   Updated: 2020/06/17 18:44:05 by ambelghi         ###   ########.fr       */
+/*   Updated: 2020/06/17 21:46:27 by ambelghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 #include <struct.h>
 #include <sh.h>
 #include "ft_printf.h"
+#include "get_next_line.h"
+#include "var.h"
 
 uint8_t static	fc_erro(char *s)
 {
 	char    *usage;
 	char	*usage2;
 
-    usage = " : fc [-r][-e editor] [first[last]]\n        ";
+	usage = " : fc [-r][-e editor] [first[last]]\n        ";
 	usage2 = "fc -l[-nr] [first[last]]\n        fc -s[old=new] [first]\n";
-    ft_dprintf(2, "fc: illegal option -- %s\nusage%s%s", s, usage, usage2);
-    return (-1);
+	ft_dprintf(2, "fc: invalid option -- %s\nusage%s%s", s, usage, usage2);
+	return (-1);
 }
 
 int		range_error(void)
@@ -33,19 +35,48 @@ int		range_error(void)
 	return (-1);
 }
 
+static t_point         get_range(int8_t *fl, char **av, int ac, t_cfg *cfg)
+{
+    t_point t;
+
+    if (av && av[ac])
+    {
+        if (!av[ac + 1] || (t.y = ft_atoi(av[ac + 1])) == 0 || t.y > cfg->hist_nb)
+            t.y = cfg->hist_nb - 1;
+        if (t.y < 0)
+            t.y = cfg->hist_nb + t.y;
+        if ((t.x = ft_atoi(av[ac])) == 0 || t.x > cfg->hist_nb)
+            t.x = cfg->hist_nb - 1;
+        if (t.x < 0)
+            t.x = cfg->hist_nb + t.x;
+        if (t.x > t.y)
+        {
+            t = (t_point){t.y, t.x};
+            *fl = (*fl & 8 ? *fl ^ 8 : *fl | 8);
+        }
+    }
+    else
+    {
+        (void)fl;
+        t.y = cfg->hist_nb - 1;
+        t.x = (cfg->hist_nb <= 15 ? 1 : cfg->hist_nb - 15);
+    }
+    return (t);
+}
+
 static uint8_t  check_opt(t_process *p, int32_t *ac)
 {
-    int32_t     i;
+	int32_t     i;
 	int			z;
-    int8_t      ret;
-    int8_t      flags;
+	int8_t      ret;
+	int8_t      flags;
 
-    flags = 0;
-    i = 0;
+	flags = 0;
+	i = 0;
 	char *fl = "elnrs";
-    while ((ret = ft_getopt(ac, &i, p->av, fl)) != -1)
-    {
-        if (ret == '?')
+	while ((ret = ft_getopt(ac, &i, p->av, fl)) != -1)
+	{
+		if (ret == '?')
 		{
 			z = i;
 			while (p->av[*ac][z])
@@ -58,70 +89,44 @@ static uint8_t  check_opt(t_process *p, int32_t *ac)
 		flags |= (char)ret == 'n' ? 4 : 0;
 		flags |= (char)ret == 'r' ? 8 : 0;
 		flags |= (char)ret == 's' ? 16 : 0;
-    }
-    return (flags);
+		if ((flags & 1 && flags & 2))
+			return (fc_erro((!p->av[*ac] ? p->av[*ac - 1] : p->av[*ac])));
+	}
+	return (flags);
 }
 
-char			*create_histfile()
+char			*get_histfilename()
 {
 	if (access("/tmp", W_OK | R_OK) < 0)
 		return (ft_strdup("./edit_hist"));
 	else
 		return (ft_strdup("/tmp/edit_hist"));
 }
-/*
-char			*init_file(t_cfg *cfg, t_point cmd_nb)
+
+char			*create_tmphist(int8_t *fl, char **av, int ac)
 {
-	t_dlist	*hist;
-	char	*filename;
-	int		i;
+	t_point	range;
+	char	*file;
 	int		fd;
-
-	filename = create_histfile();
-	hist = cfg->history;
-	i = 0;
-	if ((fd = open(filename, O_CREAT | O_RDONLY | O_WRONLY, 0666)) < 0)
+	int		order;
+	char	*key;
+	
+	file = get_histfilename();
+	if (!file || (fd = open(file, O_CREAT | O_RDONLY | O_WRONLY, 0666)) < 0)
 		return (NULL);
-	while (cfg->hist_nb - i++ != cmd_nb.x && hist->prev)
-		hist = hist->prev;
-	i = 0;
-	while (i++ < cmd_nb.y && hist)
+	range = get_range(fl, av, ac, cfg_shell());
+	if (*fl & 8)
+		range = (t_point){range.y, range.x};
+	order = (range.x > range.y ? -1 : 1);
+	while (range.x - order != range.y && range.x > 0)
 	{
-		ft_putstr_fd((char *)hist->data, fd);
+		key = ft_itoa(range.x);
+		ft_putstr_fd((char *)ft_hash_lookup(cfg_shell()->hist_map, key), fd);
 		ft_putstr_fd("\n", fd);
-		hist = hist->next;
+		ft_strdel(&key);
+		range.x += order;
 	}
-	close(fd);
-	return (filename);
-}
-*/
-static t_point         get_range(int8_t *fl, char **av, int ac, t_cfg *cfg)
-{
-    t_point t;
-
-    if (av && av[ac])
-    {
-        if (!av[ac + 1] || (t.y = ft_atoi(av[ac + 1])) == 0 || t.y > cfg->hist_nb)
-            t.y = cfg->hist_nb - 1;
-        if (t.y < 0)
-            t.y = cfg->hist_nb + t.y;
-		if ((t.x = ft_atoi(av[ac])) == 0 || t.x > cfg->hist_nb)
-            t.x = cfg->hist_nb - 1;
-        if (t.x < 0)
-            t.x = cfg->hist_nb + t.x;
-        if (t.x > t.y)
-        {
-            t = (t_point){t.y, t.x};
-            *fl = (*fl & 8 ? *fl ^ 8 : *fl | 8);
-        }
-    }
-    else
-    {
-		(void)fl;
-        t.y = cfg->hist_nb - 1;
-        t.x = (cfg->hist_nb <= 15 ? 1 : cfg->hist_nb - 15);
-    }
-    return (t);
+	return (file);
 }
 
 static int		check_range(char **av, int ac)
@@ -157,12 +162,12 @@ static int		ft_intlen(int nb)
 	if (n == 0)
 		return (1);
 	ret = 0;
-	while (n && ret++)
+	while (n && ret++ >= 0)
 		n /= 10;
 	return (ret);
 }
 
-void			print_hist(int8_t *fl, char **av, int ac)
+int			print_hist(int8_t *fl, char **av, int ac)
 {
 	t_point	range;
 	int		order;
@@ -178,14 +183,76 @@ void			print_hist(int8_t *fl, char **av, int ac)
 		while (range.x - order != range.y && range.x > 0)
 		{
 			if (!(*fl & 4))
-                ft_putnbr(range.x);
-            putchar_n(' ', (!(*fl & 4) ? 12 - ft_intlen(range.x) : 12));
-            key = ft_itoa(range.x);
+				ft_putnbr(range.x);
+			putchar_n(' ', (!(*fl & 4) ? 12 - ft_intlen(range.x) : 12));
+			key = ft_itoa(range.x);
 			ft_putendl((char *)ft_hash_lookup(cfg->hist_map, key));
 			ft_strdel(&key);
 			range.x += order;
 		}
+		return (1);
 	}
+	return (-1);
+}
+
+void		exec_hist(char *file)
+{
+	t_lexer     lexer;
+	t_parser    parser;
+	char		*cmd;
+	char		*hist_cmd;
+	int			fd;
+	int			ret;
+	char		*nb;
+
+	if (file && (fd = open(file, O_RDONLY)) >= 0)
+	{
+		while (get_next_line(fd, &cmd) > 0)
+		{
+			hist_cmd = ft_strdup(cmd);
+			if ((ret = lexer_routine(&cmd, &lexer)) <= 0
+					|| (ret = parser_routine(&lexer, &parser)) <= 0
+					|| (ret = eval_routine(&parser)) <= 0)
+				if (ret == -1)
+					break ;
+			if (ft_atoi(find_var_value(cfg_shell()->sp, "?")) == 0)
+			{
+				cfg_shell()->hist_nb += 1;
+				nb = ft_itoa(cfg_shell()->hist_nb);
+				ft_dlstaddtail(&cfg_shell()->history, ft_dlstnew(hist_cmd, 0));
+				ft_hash_add(cfg_shell()->hist_map, nb, ft_strdup(hist_cmd), NULL);
+				ft_strdel(&nb);
+			}
+		}
+	}
+}
+
+int			edit_hist(int8_t *fl, char **av, int ac)
+{
+	char	*hist;
+	char	*cmd;
+	t_lexer     lexer;
+	t_parser    parser;
+	int			ret;
+
+	if ((hist = create_tmphist(fl, av, (av[ac] ? ac + 1 : ac))))
+	{
+		if (!av[ac] && !find_var_value(cfg_shell()->sp, "FCEDIT"))
+			ft_asprintf(&cmd, "%s %s", "ed", hist);
+		else if (!av[ac])
+			ft_asprintf(&cmd, "%s %s", find_var_value(cfg_shell()->sp,
+						"FCEDIT"), hist);
+		else
+			ft_asprintf(&cmd, "%s %s", av[ac], hist);
+		if ((ret = lexer_routine(&cmd, &lexer)) <= 0
+				|| (ret = parser_routine(&lexer, &parser)) <= 0
+				|| (ret = eval_routine(&parser)) <= 0)
+			return (0);
+		exec_hist(hist);
+		ft_strdel(&hist);
+		return (1);
+	}
+	return (0);
 }
 
 uint8_t			ft_fc(t_job *j, t_process *p)
@@ -195,11 +262,13 @@ uint8_t			ft_fc(t_job *j, t_process *p)
 
 	ac = 1;
 	if ((fl = check_opt(p, &ac)) < 0)
-        return (FAILURE);
+		return (FAILURE);
 	if (j && p)
 	{
-		if (fl & 2)
-			print_hist(&fl, p->av, ac);
+		if (fl & 1 && !edit_hist(&fl, p->av, ac))
+			return (FAILURE);
+		if (fl & 2 && !print_hist(&fl, p->av, ac))
+			return (FAILURE);
 		return (SUCCESS);
 	}
 	return (SUCCESS);
