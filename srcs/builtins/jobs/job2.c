3e65427e307d5b7fec3ae915d227589225b1260b
walden <waalden@pm.mb>
2020-06-20 20:15:12 +0200

    jobs basique fait, reste argument

--- srcs/builtins/jobs/job2.c
+++ srcs/builtins/jobs/job2.c
@@ -11,14 +11,44 @@
 # define begin_job %string
 # define contain_job %?string
 
-static void			print_this_job(t_job *j, uint8_t curr, int16_t opt)
+
+static int8_t			error(void)
 {
-	if (curr > 45)
-		curr = 32;
-	ft_printf("%s %d %d %d\n", j->cmd, j->id, curr, opt);
+	ft_printf("error\n");
+	return (FAILURE);
 }
 
-static void			print_all_jobs(t_cfg *shell, t_list *jobs, int16_t opt)
+static char			*state(t_job *j, uint8_t opt)
+{
+	char	*tab[4];
+	int16_t	s_ret;
+
+	tab[0] = S_SIGSTOP;
+	tab[1] = S_SIGTSTP;
+	tab[2] = S_SIGTTIN;
+	tab[3] = S_SIGTTOU;
+	if (j->status & RUNNING)
+		return ("Running");
+	s_ret = j->ret - 19 - 128;
+	if (s_ret > -1 && s_ret < 4 && !opt)
+		return (tab[s_ret]);
+	if (j->status & STOPPED)
+		return ("Stopped");
+	return ("unknown");
+}
+
+static void			print_this_job(t_job *j, uint8_t curr, char opt)
+{
+	curr = (curr > 45) ? 32 : curr;
+	if (opt == 'p')
+		ft_printf("%d\n", j->pgid);
+	else if (opt == 'l')
+		ft_printf("[%d]%c %d %s \t %s\n", j->id, curr, j->pgid, state(j, 1), j->cmd);
+	else
+		ft_printf("[%d]%c %s \t %s\n", j->id, curr, state(j, 0), j->cmd);
+}
+
+static void			print_all_jobs(t_cfg *shell, t_list *jobs, char opt)
 {
 	uint8_t		i;
 	uint8_t		c;
@@ -35,23 +65,21 @@ static void			print_all_jobs(t_cfg *shell, t_list *jobs, int16_t opt)
 		if (tmp)
 			print_this_job(j, c, opt);
 	}
-	ft_printf(" %d < %d\n", i, shell->active_job);
 }
 
-
-static int16_t		jobs_opt(char **av, int *ac)
+static char		jobs_opt(char **av, int *ac)
 {
 	int32_t		i;
-	int32_t		ret;
-	uint8_t		opt;
+	int8_t		ret;
+	char		opt;
 
 	i = 0;
 	opt = 0;
-	while ((ret = ft_getopt(ac, &i, av, "pl") != -1))
+	while ((ret = ft_getopt(ac, &i, av, "pl")) != -1)
 	{
-		//if (ret == '?')
-		//	error();
-		opt = ret;
+		if (ret == '?')
+			return (error());
+		opt = ret != -1 ? ret : opt;
 	}
 	return (opt);
 }
@@ -59,13 +87,15 @@ static int16_t		jobs_opt(char **av, int *ac)
 uint8_t		ft_jobs2(t_job *j, t_process *p)
 {
 	int32_t		ac;
-	int16_t		opt;
+	char		opt;
 	t_cfg		*shell;
 
 	shell = cfg_shell();
 	(void)j;
 	ac = 1;
-	opt = jobs_opt(p->av, &ac);
+	if ((opt = jobs_opt(p->av, &ac)) == FAILURE)
+		return (FAILURE);
+	
 //	if (p->av[ac])
 //		print_job();
 //	else
